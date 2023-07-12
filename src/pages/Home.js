088bde90287df62461949a3a59c13eff3f993518
Tiago Jardim <tiagomachadojardim@gmail.com>
2023-07-11 22:20:07 -0300

    colocando o icone no topo da pagina

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,10 +8,18 @@ import "./Home.css";
 function Home (){
 return(
 
-   <div className="div-home">
-   <h1> Home</h1>
+   <div >
 
+   <div className="div-home"><h1> Home</h1></div>
+   
+   <div className="div-trending"><h1> Trending Collection</h1></div>
+   <div className="div-top"><h1> Top Creators</h1></div>
+   <div className="div-browse"><h1> Browse Categories</h1></div>
+
+   <footer></footer>
    </div>
+
+   
    
 
 );
