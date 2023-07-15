0a86aabf08c7b6f7922cb6635841a67ceae7a105
gabrieldal <88405111+gabrieldal@users.noreply.github.com>
2023-07-15 19:15:05 -0300

    começando rankings

---
+++ src/components/ConteudoTopRank.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ConteudoTop.css";
+
+const ConteudoTopRank = () => {
+  return (
+    <div className="conteudotop-conteudo">
+      <div className="conteudotop-conteudo-div">
+        <h3> Top Creators</h3>
+        <p>Checkout Top Rated Creators on the NFT Marketplace</p>
+      </div>
+    </div>
+  );
+};
+
+export default ConteudoTopRank;
--- src/pages/Rankings.js
+++ src/pages/Rankings.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./Rankings.css";
+import ConteudoTopRank from "../components/ConteudoTopRank";
 
 
 
@@ -9,7 +10,7 @@ function Rankings (){
 return(
 
    <div className="div-rankings">
-   <h1> Rankings</h1>
+   <ConteudoTopRank/>
    
       </div>
 
