16ce7856fc7611f067eeafc462af466474f9baf9
Carlos Lopes <carlos.lopes.pb@compasso.com.br>
2023-07-15 20:40:57 -0300

    create account em andamento

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,21 +1,36 @@
 import React from "react";
+import ImageAccount from "../assets/ImageCreateAccount.png";
 
 import "./Account.css";
 
-
-
-
-function Account (){
-return(
-
-<div className="div-account">
-<h1> Account</h1>
-
-   </div>
-   
-
-);
-
+function Account() {
+  return (
+    <div className="div-account">
+      <div className="image-wrapper">
+        <img
+          className="image-account"
+          src={ImageAccount}
+          alt="Two persons in space"
+        />
+      </div>
+      <form className="form-create-account">
+        <div className="form-text">
+          <h2>Create Account</h2>
+          <p>
+            Welcome! Enter your details and start creating, collecting and
+            selling Nfts.
+          </p>
+        </div>
+        <div className="form-style">
+          <input placeholder="Username" type="text" />
+          <input placeholder="Email Address" type="email" />
+          <input placeholder="Password" type="password" />
+          <input placeholder="Confirm Password" type="password" />
+          <button>Create Account</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Account;
