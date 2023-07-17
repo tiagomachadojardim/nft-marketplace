a0547bcdcd1b8ee71f68d4e660aa638b0bcc3223
gabrieldal <88405111+gabrieldal@users.noreply.github.com>
2023-07-17 17:09:51 -0300

    responsivo

--- src/components/ConteudoRankingsToday.js
+++ src/components/ConteudoRankingsToday.js
@@ -7,9 +7,12 @@ const ConteudoRankingsToday = () => {
     <div className="conteudoranktoday-conteudo">
       <div className="conteudoranktoday-conteudo-div">
         
-        <p className="conteudotoday">Today</p>
-        <p>This Week</p>
-        <p>This Mounth</p>
+        <p className="conteudotoday sec">Today</p>
+        <p className="sec">This Week</p>
+        <p className="sec">This Mounth</p>
+        <p className="conteudotoday sec2">1d</p>
+        <p className="sec2">7d</p>
+        <p className="sec2">30d</p>
         <p>All Time</p>
 
       </div>
